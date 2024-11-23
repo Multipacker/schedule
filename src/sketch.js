a7a57f5abda0cddb586a6da19fc91819379a1cac
Simon Renhult <simon.renhult@hotmail.se>
2024-11-23 18:48:02 +0100

    Refactor: Utilise promises more

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,44 @@
 import fs   from "node:fs/promises";
 import ical from "ical";
 
-const url = "https://cloud.timeedit.net/chalmers/web/public/ri617QQQY83Zn4Q5868548Z5y6Z55.ics";
+const calendars = [
+    {
+        url: "https://cloud.timeedit.net/chalmers/web/public/ri617QQQY83Zn4Q5868548Z5y6Z55.ics",
+        name: "Chalmers",
+        rules: [
+            {
+                include: {
+                    other: [ "Laboration" ],
+                },
+                exclude: {
+                    dates: [ "2024-11-25 17:15", "2024-12-13 17:15" ],
+                },
+            },
+            {
+                include: {
+                    codes: [ "DAT038", "TIF085" ],
+                },
+                exclude: {
+                    codes: [ "DAT038", "TIF085" ],
+                },
+                description: "$heading",
+                summary:     "$other",
+                other_sep:   ", ",
+                room:        "$room: $building våning $floor",
+                room_sep:    ", ",
+                rooms:       "$rooms",
+            },
+            {
+                other_sep:   ", ",
+                summary:     "$heading $other",
+                description: "",
+                room:        "$room: $building våning $floor",
+                room_sep:    "\n",
+                rooms:       "$rooms",
+            },
+        ]
+    },
+];
 
 const getCalendar = async url => {
     const calendar = url.substring(url.lastIndexOf("/") + 1);
@@ -9,158 +46,126 @@ const getCalendar = async url => {
     const filename = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${calendar}`;
 
     return fs.readFile(filename, "utf8")
-        .catch(_ => fetch(url))
-        .then(response => response.text())
-        .then(data => {
-            fs.writeFile(filename, data);
-            return data;
-        });
+        .catch(_ => fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                fs.writeFile(filename, data);
+                return data;
+            })
+        );
 };
 
-const data = await getCalendar(url);
+const parseEvents = data => Object.values(ical.parseICS(data))
+    .filter(event => event.type === "VEVENT")
+    .map(event => {
+        const names = event.summary
+            .split(",")
+            .map(part => part.trim().match("Kurskod: ([^.]+). Kursnamn: (.+)"))
+            .filter(match => match != null)
+            .map(match => ({ code: match[1], name: match[2], }));
 
-let events = [];
-const ical_events = ical.parseICS(data);
-for (const i in ical_events) {
-    const event = ical_events[i];
-    if (event.type !== "VEVENT") {
-        continue;
+        const heading = (event.summary.match("Rubrik: ([^,]+)") ?? ["", ""])[1];
+
+        const other = event.summary
+            .split(",")
+            .filter(part => !part.includes(":"))
+            .map(part => part.trim());
+
+        const locations = event.location
+            .split("\n")
+            .map(line => line.trim().match("^([^.]+). Byggnad: ([^.]+)(. Våningsplan: (.+))?"))
+            .filter(match => match != null)
+            .map(match => ({ room: match[1], building: match[2], floor: match[4] ?? "", }));
+
+        return {
+            names:     names,
+            heading:   heading,
+            other:     other,
+            locations: locations,
+            start:     event.start,
+            end:       event.end,
+        };
+    });
+
+const matchesFilter = (event, filters) => {
+    let passes = true;
+
+    if (filters.codes !== undefined) {
+        passes &= event.names.some(entry => filters.codes.includes(entry.code));
     }
 
-    const names = event.summary
-        .split(",")
-        .map(part => part.trim().match("Kurskod: ([^.]+). Kursnamn: (.+)"))
-        .filter(match => match != null)
-        .map(match => ({ code: match[1], name: match[2], }));
+    if (filters.other !== undefined) {
+        passes &= event.other.some(other => filters.other.includes(other));
+    }
 
-    const heading = (event.summary.match("Rubrik: ([^,]+)") ?? ["", ""])[1];
+    if (filters.dates !== undefined) {
+        passes &= filters.dates.map(date => Date.parse(date)).some(date => event.start <= date && date < event.end)
+    }
 
-    const other = event.summary
-        .split(",")
-        .filter(part => !part.includes(":"))
-        .map(part => part.trim());
+    return passes;
+};
 
-    const locations = event.location
-        .split("\n")
-        .map(line => line.trim().match("^([^.]+). Byggnad: ([^.]+)(. Våningsplan: (.+))?"))
-        .filter(match => match != null)
-        .map(match => ({ room: match[1], building: match[2], floor: match[4] ?? "", }));
+const matchesRule = (event, rule) => {
+    let passes = true;
 
-    events.push({
-        names:     names,
-        heading:    heading,
-        other:     other,
-        locations: locations,
-        start:     event.start,
-        end:       event.end,
-    });
-}
+    if (rule.include !== undefined) {
+        passes &= matchesFilter(event, rule.include);
+    }
 
-const rules = [
-    {
-        include: {
-            other: [ "Laboration" ],
-        },
-        exclude: {
-            dates: [ "2024-11-25 17:15", "2024-12-13 17:15" ],
-        },
-    },
-    {
-        include: {
-            codes: [ "DAT038", "TIF085" ],
-        },
-        exclude: {
-            codes: [ "DAT038", "TIF085" ],
-        },
-        description: "$heading",
-        summary: "$other",
-        other_sep: ", ",
-        room: "$room: $building våning $floor",
-        room_sep: ", ",
-        rooms: "$rooms",
-    },
-    {
-        other_sep: ", ",
-        summary: "$heading $other",
-        description: "",
-        room: "$room: $building våning $floor",
-        room_sep: "\n",
-        rooms: "$rooms",
-    },
-];
+    if (rule.exclude !== undefined) {
+        passes &= !matchesFilter(event, rule.exclude);
+    }
 
-console.log(`BEGIN:VCALENDAR`);
-console.log(`VERSION:2.0`);
-console.log(`METHOD:PUBLISH`);
-console.log(`CALSCALE:GREGORIAN`);
-for (const event of events) {
-    const matches = filters => {
-        if (filters === undefined) {
-            return true;
-        }
+    return passes;
+};
 
-        let passes = true;
+calendars.forEach(({ url, name, rules, }) => getCalendar(url)
+    .then(parseEvents)
+    .then(events => events
+        .map(event => ({ event: event, rule: rules.find(rule => matchesRule(event, rule)), }))
+        .filter(({ rule, }) => rule !== undefined && !(rule.summary === undefined && rule.description === undefined && rule.rooms === undefined))
+        .map(({ event, rule, }) => {
+            const rooms = event.locations
+                .map(entry => rule.room
+                    .replace("$room",     entry.room)
+                    .replace("$floor",    entry.floor)
+                    .replace("$building", entry.building))
+                .join(rule.room_sep);
+            const other = event.other.join(rule.other_sep);
 
-        if (filters.codes !== undefined) {
-            passes &= event.names.some(entry => filters.codes.includes(entry.code));
-        }
+            const fillPattern = pattern => pattern
+                .replace("$heading", event.heading)
+                .replace("$other",   other)
+                .replace("$rooms",   rooms);
 
-        if (filters.other !== undefined) {
-            passes &= event.other.some(other => filters.other.includes(other));
-        }
-
-        if (filters.dates !== undefined) {
-            passes &= filters.dates.map(date => Date.parse(date)).some(date => event.start <= date && date < event.end)
-        }
-
-        return passes;
-    };
-
-    const passing_index = rules.findIndex(rule => {
-        let passes = true;
-
-        if (rule.include !== undefined) {
-            passes &= matches(rule.include);
-        }
-
-        if (rule.exclude !== undefined) {
-            passes &= !matches(rule.exclude);
-        }
-
-        return passes;
-    });
-
-    if (passing_index !== -1) {
-        const rule = rules[passing_index];
-
-        if (rule.summary === undefined && rule.description === undefined && rule.rooms === undefined) {
-            continue;
-        }
-
-        const all_rooms = event.locations.map(entry => rule.room.replace("$room", entry.room).replace("$floor", entry.floor).replace("$building", entry.building)).join(rule.room_sep);
-        const all_other = event.other.join(rule.other_sep);
-
+            return {
+                summary:     fillPattern(rule.summary),
+                description: fillPattern(rule.description),
+                rooms:       fillPattern(rule.rooms),
+                start:       event.start,
+                end:         event.end,
+            };
+        })
+    )
+    .then(events => {
         // TODO(simon): Make sure this escapes everything that needs to be.
-        const escape = string => {
-            return string.replace(",", "\\,").replace("\n", "\\n");
-        };
+        const escape = string => string.replace(",", "\\,").replace("\n", "\\n");
+        const formatDate = date => date.toISOString().replace(/[-:]|(\.\d{3})/g, "");
 
-        const fill_rule = rule => {
-            return rule.replace("$heading", event.heading).replace("$other", all_other).replace("$rooms", all_rooms);
-        };
-
-        const summary     = escape(fill_rule(rule.summary));
-        const description = escape(fill_rule(rule.description));
-        const rooms       = escape(fill_rule(rule.rooms));
-
-        // NOTE(simon): Serialize
-        console.log(`BEGIN:VEVENT`);
-        console.log(`DTSTART:${event.start.toISOString().replace(/[-:]|(\.\d{3})/g, "")}`);
-        console.log(`DTEND:${event.end.toISOString().replace(/[-:]|(\.\d{3})/g, "")}`);
-        console.log(`SUMMARY:${summary}`);
-        console.log(`DESCRIPTION:${description}`);
-        console.log(`LOCATION:${rooms}`);
-        console.log(`END:VEVENT`);
-    }
-}
-console.log(`END:VCALENDAR`);
+        let output = "";
+        output += "BEGIN:VCALENDAR\n";
+        output += "VERSION:2.0";
+        output += "METHOD:PUBLISH\n";
+        output += "CALSCALE:GREGORIAN\n";
+        events.forEach(({ summary, description, rooms, start, end, }) => {
+            output += "BEGIN:VEVENT\n";
+            output += `DTSTART:${formatDate(start)}\n`;
+            output += `DTEND:${formatDate(end)}\n`;
+            output += `SUMMARY:${escape(summary)}\n`;
+            output += `DESCRIPTION:${escape(description)}\n`;
+            output += `LOCATION:${escape(rooms)}\n`;
+            output += "END:VEVENT\n";
+        });
+        output += "END:VCALENDAR\n";
+        fs.writeFile(`${name}.ical`, output);
+    }));
