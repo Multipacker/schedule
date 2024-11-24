1110138d4f7abe1ec60315984e24d38f76b0e5ec
Simon Renhult <simon.renhult@hotmail.se>
2024-11-24 09:30:12 +0100

    Dev: Make us conform with the iCal speification

--- src/sketch.js
+++ src/sketch.js
@@ -2,15 +2,26 @@ import fsSync from "node:fs";
 import fs     from "node:fs/promises";
 import path   from "path";
 import ical   from "ical";
+import os     from "os";
 
 const config = await fs.readFile("config.json")
     .then(
         JSON.parse,
-        error => {}
-    );
+        error => {
+            console.log(`Could not load configuration file 'config.json'. Using default configuration\n\t${error}`);
+            return {};
+        }
+    )
+    // NOTE(simon): Fill in defaults for values that are not specified.
+    .then(data => ({
+        cacheClearInterval: data.cacheClearInterval ?? 60 * 24,
+        cacheDirectory:     data.cacheDirectory     ?? ".",
+        calendarDirectory:  data.calendarDirectory  ?? ".",
+        generateInterval:   data.generateInterval   ?? 60 * 24,
+    }));
 
 const ensureConfigDirectory = directory => {
-    if (directory !== undefined && !fsSync.existsSync(directory)) {
+    if (!fsSync.existsSync(directory)) {
         fsSync.mkdirSync(directory, { recursive: true, });
     }
 };
@@ -21,7 +32,7 @@ ensureConfigDirectory(config.calendarDirectory);
 const getCalendar = async url => {
     const calendar = url.substring(url.lastIndexOf("/") + 1);
     const date     = new Date();
-    const filename = path.join(config.cacheDirectory ?? ".", `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${calendar}`);
+    const filename = path.join(config.cacheDirectory, `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${calendar}`);
 
     return fs.readFile(filename, "utf8")
         .catch(_ => {
@@ -107,16 +118,16 @@ const processCalendar = ({ url, name, rules, }) => getCalendar(url)
         .map(({ event, rule, }) => {
             const rooms = event.locations
                 .map(entry => rule.room
-                    .replace("$room",     entry.room)
-                    .replace("$floor",    entry.floor)
-                    .replace("$building", entry.building))
+                    .replace(/\$room/g,     entry.room)
+                    .replace(/\$floor/g,    entry.floor)
+                    .replace(/\$building/g, entry.building))
                 .join(rule.room_sep);
             const other = event.other.join(rule.other_sep);
 
             const fillPattern = pattern => pattern
-                .replace("$heading", event.heading)
-                .replace("$other",   other)
-                .replace("$rooms",   rooms);
+                .replace(/\$heading/g, event.heading)
+                .replace(/\$other/g,   other)
+                .replace(/\$rooms/g,   rooms);
 
             return {
                 summary:     fillPattern(rule.summary),
@@ -128,27 +139,34 @@ const processCalendar = ({ url, name, rules, }) => getCalendar(url)
         })
     )
     .then(events => {
-        // TODO(simon): Make sure this escapes everything that needs to be.
-        const escape = string => string.replace(",", "\\,").replace("\n", "\\n");
+        const escape = string => string.replace(/\\/g, "\\\\").replace(/;/g, "\\;").replace(/,/g, "\\,").replace(/\n/g, "\\n");
         const formatDate = date => date.toISOString().replace(/[-:]|(\.\d{3})/g, "");
+        const generationTime = formatDate(new Date());
+        const hostID = escape(os.hostname());
 
         let output = "";
-        output += "BEGIN:VCALENDAR\n";
-        output += "VERSION:2.0";
-        output += "METHOD:PUBLISH\n";
-        output += "CALSCALE:GREGORIAN\n";
-        events.forEach(({ summary, description, rooms, start, end, }) => {
-            output += "BEGIN:VEVENT\n";
-            output += `DTSTART:${formatDate(start)}\n`;
-            output += `DTEND:${formatDate(end)}\n`;
-            output += `SUMMARY:${escape(summary)}\n`;
-            output += `DESCRIPTION:${escape(description)}\n`;
-            output += `LOCATION:${escape(rooms)}\n`;
-            output += "END:VEVENT\n";
+        output += "BEGIN:VCALENDAR\r\n";
+        output += "VERSION:2.0\r\n";
+        output += "METHOD:PUBLISH\r\n";
+        output += `X-WR-CALNAME:${escape(name)}`
+        output += `X-PUBLISHED-TTL:P${config.generateInterval}M\r\n`
+        output += "CALSCALE:GREGORIAN\r\n";
+        output += `PRODID:Schedule\r\n`
+        events.forEach(({ summary, description, rooms, start, end, }, index) => {
+            output += "BEGIN:VEVENT\r\n";
+            output += `DTSTART:${formatDate(start)}\r\n`;
+            output += `DTEND:${formatDate(end)}\r\n`;
+            output += `SUMMARY:${escape(summary)}\r\n`;
+            output += `DESCRIPTION:${escape(description)}\r\n`;
+            output += `LOCATION:${escape(rooms)}\r\n`;
+            output += `UID:${generationTime}-${escape(name)}-${index}@${hostID}\r\n`;
+            output += `DTSTAMP:${generationTime}\r\n`;
+            output += `LAST-MODIFIED:${generationTime}\r\n`;
+            output += "END:VEVENT\r\n";
         });
-        output += "END:VCALENDAR\n";
+        output += "END:VCALENDAR\r\n";
 
-        const filename = path.join(config.calendarDirectory ?? ".", `${name}.ical`);
+        const filename = path.join(config.calendarDirectory, `${name}.ical`);
         console.log(`Saving ${filename}`);
         fs.writeFile(filename, output);
     });
@@ -170,34 +188,26 @@ const clearCache = () => {
     console.log("Clearing cache");
     const date           = new Date();
     const filenamePrefix = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-`;
-    const directory      = config.cacheDirectory ?? ".";
     const filenameRegex  = new RegExp(/^\d+-\d{1,2}-\d{1,2}-.+\.ics$/);
 
-    fs.readdir(directory).then(
+    fs.readdir(config.cacheDirectory).then(
         files => {
             files
                 .filter(file => !file.startsWith(filenamePrefix) && filenameRegex.test(file))
                 .forEach(file => {
-                    const filename = path.join(directory, file);
+                    const filename = path.join(config.cacheDirectory, file);
                     console.log(`Deleting ${filename}`);
                     fs.unlink(filename).catch(error => console.log(`Could not delete ${filename}\n\t${error}`));
                 });
         },
         error => {
-            console.log(`Could not read cached files in '${directory}\n\t${error}'`);
+            console.log(`Could not read cached files in '${config.cacheDirectory}\n\t${error}'`);
         }
     );
 };
 
-setInterval(
-    processCalendars,
-    1000 * 60 * (config.generateInterval ?? 60 * 24)
-);
-
-setInterval(
-    clearCache,
-    1000 * 60 * (config.cacheClearInterval ?? 60 * 24)
-);
+setInterval(processCalendars, 1000 * 60 * config.generateInterval);
+setInterval(clearCache, 1000 * 60 * config.cacheClearInterval);
 
 processCalendars();
 fsSync.watchFile("calendars.json", { interval: (config.configReloadInterval ?? 10) * 1000}, (current, previous) => {
