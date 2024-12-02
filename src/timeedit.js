81dd545e7b4b79727f1891e2bef0533416c592a7
Simon Renhult <simon.renhult@hotmail.se>
2024-12-02 01:17:42 +0100

    Dev: Accumulate calendars and download them as CSVs rather than iCalendars
    This gives us access to significantly more information about events.
    It's also a lot more trust worthy because the old approach tries to
    parse out information from iCalendar fields, which is error prone. Here
    we get all fields directly from the CSV.

--- src/timeedit.js
+++ src/timeedit.js
@@ -1,29 +1,259 @@
-import fs  from "node:fs";
 import { parseCsv } from "./csv.js";
 
-const data = fs.readFileSync("test.csv", "utf-8");
+const tabledata = [
+    [ 'h=t&sid=',           '6='  ],
+    [ 'objects=',           '1='  ],
+    [ 'sid=',               '2='  ],
+    [ '&ox=0&types=0&fe=0', '3=3' ],
+    [ '&types=0&fe=0',      '5=5' ],
+    [ '&h=t&p=',            '4='  ]
+];
 
-// TODO(simon): Maybe validate all data first
+const tabledataspecial = [
+    [ '=', 'ZZZX1' ],
+    [ '&', 'ZZZX2' ],
+    [ ',', 'ZZZX3' ],
+    [ '.', 'ZZZX4' ],
+    [ ' ', 'ZZZX5' ],
+    [ '-', 'ZZZX6' ],
+    [ '/', 'ZZZX7' ],
+    [ '%', 'ZZZX8' ]
+];
 
-const records = parseCsv(data, 3)
-    .map(record => {
-        console.log(record);
-        return {
-            // TODO(simon): Timezones are annoying, what do we do with them?
-            // Currently they are fixed at UTC+1.
-            start:    new Date(`${record["Startdatum"]}T${record["Starttid"]}+01:00`),
-            end:      new Date(`${record["Slutdatum"]}T${record["Sluttid"]}+01:00`),
-            // TODO(simon): Course names
-            header:   record["Rubrik"],
-            activity: record["Aktivitet"],
-            comment:  record["Bokningskommentar"],
-            // TODO(simon): Room
-            classes: record["Klass"].split(","),
-            group:   record["Undergrupp"],
-            staff:   record["Personal"].split(","),
-            // TODO(simon): Exam
-            // TODO(simon): Exam type
-        };
+const pairs = [
+    [ '=', 'Q' ],
+    [ '&', 'Z' ],
+    [ ',', 'X' ],
+    [ '.', 'Y' ],
+    [ ' ', 'V' ],
+    [ '-', 'W' ]
+];
+
+const pattern = [
+    4,  22, 5,  37, 26, 17, 33, 15, 39, 11,
+    45, 20, 2,  40, 19, 36, 28, 38, 30, 41,
+    44, 42, 7,  24, 14, 27, 35, 25, 12, 1,
+    43, 23, 6,  16, 3,  9,  47, 46, 48, 50,
+    21, 10, 49, 32, 18, 31, 29, 34, 13, 8
+];
+
+const untablespecial = result => {
+    for (let i = 0; i < 100; i++) {
+        for (const key of tabledataspecial.reverse()) {
+            result = result.replace(key[1], key[0]);
+        }
+    }
+    return result;
+}
+
+const unmodKey = ch => {
+    if (ch >= 97 && ch <= 122) {
+        return (97 + (ch - 97 + 9) % 26);
+    }
+    if (ch >= 49 && ch <= 57) {
+        return (49 + (ch - 49 + 5) % 9);
+    }
+    return ch;
+}
+
+const unscrambleChar = ch => {
+    for (const pair of pairs) {
+        if (ch === pair[1]) {
+            return pair[0];
+        }
+        if (ch === pair[0]) {
+            return pair[1];
+        }
+    }
+    return String.fromCharCode(unmodKey(ch.charCodeAt(0)));
+}
+
+const swap = (result, from, to) => {
+    if ((from < 0) || (from >= result.length)) {
+        return;
+    }
+    if ((to < 0) || (to >= result.length)) {
+        return;
+    }
+    const fromChar = result[from];
+    result[from] = result[to];
+    result[to] = fromChar;
+}
+
+const unswapPattern = result => {
+    for (let step = 0; step < result.length; step++) {
+        for (let index = pattern.length - 1; index >= 1; index -= 2) {
+            swap(
+                result,
+                pattern[index] + step * pattern.length,
+                pattern[index - 1] + step * pattern.length
+            );
+        }
+    }
+}
+
+const unswapChar = result => {
+    const split = result.split('');
+    unswapPattern(split);
+    return split.map(unscrambleChar).join('');
+}
+
+const untableshort = result => {
+    for (const key of tabledata.reverse()) {
+        result = result.replace(key[1], key[0]);
+    }
+    return result;
+}
+
+const unscramble = query => {
+    if (query.length < 2) {
+        return query;
+    }
+    if (query.substring(0, 2) === 'i=') {
+        return query;
+    }
+    let result = encodeURIComponent(query);
+    result = untablespecial(result);
+    result = unswapChar(result);
+    result = untableshort(result);
+    return decodeURIComponent(result);
+}
+
+const modKey = ch => {
+    if (ch >= 97 && ch <= 122) {
+        return (97 + (ch - 88) % 26);
+    }
+    if (ch >= 49 && ch <= 57) {
+        return (49 + (ch - 45) % 9);
+    }
+    return ch;
+}
+
+const scrambleChar = ch => {
+    for (const pair of pairs) {
+        if (ch === pair[0]) {
+            return pair[1];
+        }
+        if (ch === pair[1]) {
+            return pair[0];
+        }
+    }
+    return String.fromCharCode(modKey(ch.charCodeAt(0)));
+}
+
+const swapPattern = result => {
+    for (let step = 0; step < result.length; step++) {
+        for (let index = 1; index < pattern.length; index += 2) {
+            swap(
+                result,
+                pattern[index] + step * pattern.length,
+                pattern[index - 1] + step * pattern.length
+            );
+        }
+    }
+}
+
+const swapChar = result => {
+    const split = result.split('').map(scrambleChar);
+    swapPattern(split);
+    return split.join('');
+}
+
+const tableshort = result => {
+    for (const key of tabledata) {
+        result = result.replace(key[0], key[1]);
+    }
+    return result;
+}
+
+const tablespecial = result => {
+    for (let i = 0; i < 100; i++) {
+        for (const key of tabledataspecial) {
+            result = result.replace(key[0], key[1]);
+        }
+    }
+    return result;
+}
+
+const scramble = query => {
+    if (query.length < 2) {
+        return query;
+    }
+    if (query.substring(0, 2) === 'i=') {
+        return query;
+    }
+    let result = decodeURIComponent(query);
+    result = tableshort(result);
+    result = swapChar(result);
+    result = tablespecial(result);
+    return encodeURIComponent(result);
+}
+
+const decode = url => {
+    const lastSlash = url.lastIndexOf('/');
+    const lastDot   = url.lastIndexOf('.');
+
+    if (lastDot === -1) {
+        lastDot = url.length;
+    }
+
+    const part = url.substring(lastSlash + 1 + 2, lastDot);
+    const parameters = new Map();
+    unscramble(part).split("&").forEach(parameter => {
+        const keyValue = parameter.split("=");
+        parameters.set(keyValue[0], keyValue[1]);
     });
+    return parameters;
+}
 
-console.log(records);
+const encode = keyValues => {
+    const parameters = keyValues.map(value => value.replace(/[+]/g, ' ')).join('&');
+    const url = `https://cloud.timeedit.net/chalmers/web/public/ri${scramble(parameters)}.csv`;
+    return url;
+};
+
+const getEvents = urls => {
+    const parameters = urls.map(decode);
+    const objects = parameters.flatMap(parameters => parameters.get("objects").split(","));
+    const url = encode([ `objects=${objects.join(",")}`, "sid=3", ]);
+
+    console.log(`Downloading calendar from ${url}.`);
+
+    return fetch(url)
+        .then(response => response.text())
+        .then(data => parseCsv(data, 3))
+        .then(records => records.map(record => {
+            // TODO(simon): Maybe validate all data first
+            const codes  = record.get("Kurskod").split(",");
+            const names  = record.get("Kursnamn").split(",");
+            const rooms  = record.get("Rum").split(",");
+            const houses = record.get("Hus").split(",");
+
+            if (codes.length !== names.length) {
+                throw new Error(`The number of course codes and course names differ. ${codes.length} course codes and ${names.length} course names.`);
+            }
+
+            if (rooms.length !== houses.length) {
+                throw new Error(`The number of rooms and houses differ. ${rooms.length} houses and ${houses.length} houses.`);
+            }
+
+            return {
+                // TODO(simon): Timezones are annoying, what do we do with them?
+                // Currently they are fixed at UTC+1.
+                start:    new Date(`${record.get("Startdatum")}T${record.get("Starttid")}+01:00`),
+                end:      new Date(`${record.get("Slutdatum")}T${record.get("Sluttid")}+01:00`),
+                courses:  codes.map((code, index) => ({ code: code, name: names[index], })),
+                header:   record.get("Rubrik"),
+                activity: record.get("Aktivitet"),
+                comment:  record.get("Bokningskommentar"),
+                rooms:    rooms.map((room, index) => ({ room: room, house: houses[index], })),
+                classes:  record.get("Klass").split(","),
+                group:    record.get("Undergrupp"),
+                staff:    record.get("Personal").split(","),
+                // TODO(simon): Exam
+                // TODO(simon): Exam type
+            };
+        }));
+};
+
+export { getEvents };
